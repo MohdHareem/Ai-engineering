f51d34810625a54936a5fc4524c4ee69ce1cb9d0
Mohd Hareem <mdhareem0786@gmail.com>
2026-08-03 10:09:21 +0530

    Initial commit

---
+++ week1/day1/hello_llm.py
@@ -0,0 +1,29 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key is missing")
+
+
+client=Groq(api_key=my_api_key)
+
+model="llama-3.3-70b-versatile"
+role="user"
+prompt="who is the ceo of google"
+message={
+    "role":role,
+    "content":prompt
+}
+messages=[message]
+response=client.chat.completions.create(model=model,messages=messages)
+ans=response.choices[0].message.content
+print(ans)
+
+
+
